a9ca388d0f5defcf8324eb86da8b5ab403e00ffc
KoushikReddyB <2200090018@kluniversity.in>
2023-09-10 08:33:11 +0530

    added customUser in admin panel

--- Studentlearningmanagementsystem/app/models.py
+++ Studentlearningmanagementsystem/app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    USER_CHOICES = (
+        (1, 'HOD'),
+        (2, 'STAFF'),
+        (3, 'STUDENT'),
+    )
+    user_type = models.CharField(choices=USER_CHOICES, max_length=50, default=1)
+    profile_pic = models.ImageField(upload_to='media/profile_pics')
