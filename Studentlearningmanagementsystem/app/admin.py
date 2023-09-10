c0d264487eeb1077b80fc9b1d9c7fff8f39e4834
KoushikReddyB <2200090018@kluniversity.in>
2023-09-10 11:31:46 +0530

    ++ changes in header.html

--- Studentlearningmanagementsystem/app/admin.py
+++ Studentlearningmanagementsystem/app/admin.py
@@ -2,9 +2,9 @@ from django.contrib import admin
 from .models import *
 from django.contrib.auth.admin import UserAdmin
 # Register your models here.
-'''
+
 class UserModel(UserAdmin):
     list_display = ['username', 'user_type']
 admin.site.register(CustomUser, UserModel)
-'''
-admin.site.register(CustomUser)
+
+# admin.site.register(CustomUser)
