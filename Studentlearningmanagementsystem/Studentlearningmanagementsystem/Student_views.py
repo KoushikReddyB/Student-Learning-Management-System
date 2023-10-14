772b7ab3093fa64e51eef1f283fc3cb80edca7de
KoushikReddyB <2200090018@kluniversity.in>
2023-10-15 03:00:49 +0530

    fixed the bugs

--- Studentlearningmanagementsystem/Studentlearningmanagementsystem/Student_views.py
+++ Studentlearningmanagementsystem/Studentlearningmanagementsystem/Student_views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required
-from app.models import CustomUser, Staff_Feedback, Staff_Leave, Student_Feedback, Student_Leave, Student_Notifications
-from app.models import Program, Session_Year, Student, Staff, Course, Staff_Notifications
+from app.models import CustomUser, Student_Feedback, Student_Leave, Student_Notifications
+from app.models import Program, Session_Year, Student
 from django.contrib import messages
 
 @login_required(login_url='/')
